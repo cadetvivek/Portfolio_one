8d3a8627d8b860cdf1b415adb0092aad0c690bab
cadetvivek <kushwahvivek805@gmail.com>
2024-04-29 11:16:40 +0530

    update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,7 +9,7 @@ const About = () => {
     >
       <div
         className="max-w-screen-lg p-4 mx-auto flex flex-col
-        justify-center w-full"
+        justify-center w-full h-full"
       >
         <div className="pb-8">
           <p
@@ -19,7 +19,7 @@ const About = () => {
             About
           </p>
         </div>
-        <p className="text-xl mt-8 lg:mt-20">
+        <p className="text-xl mt-5">
         I'm Vivek, a Full Stack Web Developer
         good in both front-end and back-end technologies.
          With 1200+ hours of coding practice,
@@ -31,7 +31,21 @@ const About = () => {
              Explore my projects to see how I turn ideas 
              into dynamic web solutions.
         </p>
-       
+        <br />
+       <p className="text-xl">
+       As a Full Stack Web Developer, 
+       I'm proficient in both front-end and back-end technologies.
+        With over 1200 hours of coding practice and 450+ hours dedicated 
+        to mastering Data Structures and Algorithms, 
+        I specialize in crafting efficient solutions.
+         My commitment extends beyond technical prowess,
+          investing 100 hours in soft skill development 
+          to ensure effective communication and collaboration.
+           I honed my skills further through a course at
+            Sharpener in Bengaluru, Karnataka. 
+            Explore my projects to witness how I transform ideas
+             into dynamic web solutions.
+       </p>
       </div>
     </div>
   );
